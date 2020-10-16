8e7b7ce635d7f4ffa078e4ce3e57e5e9338c2e19
wyl <18395585578@163.com>
2020-10-16 18:59:09 +0800

    单元测试，测试数据库访问结果

--- RequestLoginExample/src/test.java
+++ RequestLoginExample/src/test.java
@@ -14,7 +14,7 @@ public class test {
         //建立测试查询对象
         User loginUser = new User();
         loginUser.setUsername("yuye");
-        loginUser.setPassword("123123");
+        loginUser.setPassword("123");
 
         UserDao userDao = new UserDao();
         User user = userDao.login(loginUser);
